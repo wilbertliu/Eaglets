98d11ab67a482a4e5e866981375d3dcdce112f58
Wilbert Liu <wilbertliu@gmail.com>
2017-09-12 15:43:34 +0700

    Add simple tweet posting

--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/wilbertliu/Eaglets/credentials"
 )
 
 func main() {
+	// Setup the Twitter client.
+	anaconda.SetConsumerKey(credentials.ConsumerKey)
+	anaconda.SetConsumerSecret(credentials.ConsumerSecret)
+	twitterAPI := anaconda.NewTwitterApi(credentials.AccessToken, credentials.AccessTokenSecret)
+
+	// Read the input from user.
 	reader := bufio.NewReader(os.Stdin)
 	text, err := reader.ReadString('\n')
 
@@ -16,6 +25,6 @@ func main() {
 		return
 	}
 
-	// Otherwise print the user input.
-	fmt.Println(text)
+	// Otherwise post status update on Twitter.
+	twitterAPI.PostTweet(text, nil)
 }

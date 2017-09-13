c9f4802bd9776b39a954ad1ce578699eb202bc68
Wilbert Liu <wilbertliu@gmail.com>
2017-09-13 09:56:23 +0700

    Add tweetstorm capability

--- main.go
+++ main.go
@@ -3,28 +3,91 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net/url"
 	"os"
 
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/wilbertliu/Eaglets/credentials"
 )
 
-func main() {
+func readTweets() ([]string, error) {
+	// Make slice to store tweets.
+	tweets := make([]string, 0)
+
+	// Read tweets from user.
+	reader := bufio.NewReader(os.Stdin)
+	isStillReadingTweets := true
+	for isStillReadingTweets {
+		text, err := reader.ReadString('\n')
+
+		// Return error immediately if any.
+		if err != nil {
+			return nil, err
+		}
+
+		// Append tweet to tweets slice if tweet is not empty.
+		// Otherwise, stop reading tweets.
+		if text != "\n" {
+			tweets = append(tweets, text)
+		} else {
+			isStillReadingTweets = false
+		}
+
+		// Print newline to ease user distinguishes each tweet.
+		fmt.Println()
+	}
+
+	return tweets, nil
+}
+
+func postTweets(tweets []string) error {
 	// Setup the Twitter client.
 	anaconda.SetConsumerKey(credentials.ConsumerKey)
 	anaconda.SetConsumerSecret(credentials.ConsumerSecret)
 	twitterAPI := anaconda.NewTwitterApi(credentials.AccessToken, credentials.AccessTokenSecret)
 
-	// Read the input from user.
-	reader := bufio.NewReader(os.Stdin)
-	text, err := reader.ReadString('\n')
+	// Post tweets.
+	var postedTweet *anaconda.Tweet
+	for _, tweet := range tweets {
+		// Set the additional parameters.
+		v := url.Values{}
+		v.Set("trim_user", "true")
+		v.Set("enable_dm_commands", "false")
 
-	// If there's error, print the error and stop the execution.
-	if err != nil {
-		fmt.Println(err)
+		// When there's posted tweet, set the reply status id.
+		if postedTweet != nil {
+			v.Set("in_reply_to_status_id", postedTweet.IdStr)
+		}
+
+		// Post tweets one by one.
+		postedTweetInLoop, err := twitterAPI.PostTweet(tweet, v)
+
+		// Return error immediately if any.
+		if err != nil {
+			return err
+		}
+
+		// Store posted tweet object for further usage.
+		postedTweet = &postedTweetInLoop
+	}
+
+	return nil
+}
+
+func main() {
+	tweets, readingErr := readTweets()
+
+	// If there's error, print and stop execution.
+	if readingErr != nil {
+		fmt.Println(readingErr)
 		return
 	}
 
-	// Otherwise post status update on Twitter.
-	twitterAPI.PostTweet(text, nil)
+	postingErr := postTweets(tweets)
+
+	// If there's error, print and stop execution.
+	if postingErr != nil {
+		fmt.Println(postingErr)
+		return
+	}
 }
